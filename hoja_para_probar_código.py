5e1f781d5ae20bc48be226619cbc54645f828dcf
acasasaez <acasasae@myuax.com>
2022-04-03 12:44:38 +0200

    patrón enconteado

--- hoja_para_probar_código.py
+++ hoja_para_probar_código.py
@@ -1,9 +1,21 @@
 import re
 def validacion_email(cadena):
     text = cadena 
-    pattern = "^[a-z0-9]+[\._] ?[a-z0-9]+[@]\w+[.]w{2.3}$"
+    pattern = r"[a-z0-9-_]+[@]+[a-z]+[.]+[a-z]{2,}" #Parece que este patrón funciona para la búsqued DEL CORREO
     if (re.search(pattern, text)) is None:
         print ("mal")
     else: 
         print("okey")
-validacion_email("hola@gmail.es")
+
+#Hacemos pruebas para comprobar wue es efectivo 
+print("Prueba 1 ")
+validacion_email("vicenta@gmail.es")
+print (" ")
+print("Prueba 2")
+validacion_email("vicenta@gmailes")
+print(" ")
+print("Prueba 3")
+validacion_email("vicentagmail.es")
+print(" ")
+print("Prueba 4")
+validacion_email("vicenta")
