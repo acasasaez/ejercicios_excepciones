41455bc66d6c4f87c3af2429cad25cdb84e1da33
acasasaez <acasasae@myuax.com>
2022-04-03 13:32:51 +0200

    una vez creamos las clases y la función que compruebe nuestro email, solo queda generar el bucle que que nos dará los resultados

--- excepciones.py
+++ excepciones.py
@@ -1,4 +1,6 @@
-import re 
+from ast import Break
+import re
+from tabnanny import check 
 #Tenemos que elaborar un programa que nos indique si la dirección de correo electrónico introducida es correcta
 #para esto debemos comprobar 
 #Que la entrada es correcta
@@ -23,8 +25,25 @@ def check_email(cadena): #Definiremos una función que nos permita comprobar los
     pattern = r"[a-z0-9-_]+[@]+[a-z]+[.]+[a-z]{2,}" 
     if (re.search(pattern, text)) is None: 
         raise formato_email
-        
+
     #Que el correo introducido es el correcto
     if cadena != "vincent@eni.es":
         raise email_incorrecto
 
+a = False 
+while not a:
+    try:
+        cadena = input("Introducir correo: ")
+        check_email(cadena)
+    #si llegamos al Break quiere decir que nuestro código ha sido examinado y cumple los requisitos, 
+    #por eso nuestro programa sale del bucle
+        break
+    except entrada:
+        print("Entrada incorrecta")
+    except formato_email:
+        print ("Introduzca una dirección de correo electrónico válida. Ej: manoloperz@gmai.com")
+    except email_incorrecto:
+        print("Dirección de correo electrónico incorrecta. Acceso denegado")
+        a = True
+if not a:
+    print("Bienvenido Vicente")
