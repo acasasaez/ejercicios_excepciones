0b3b47667911d1fde435c302801eafb3ff0ed9ac
acasasaez <acasasae@myuax.com>
2022-04-03 13:13:40 +0200

    fin función para comprobar el email

--- excepciones.py
+++ excepciones.py
@@ -12,15 +12,19 @@ class formato_email (BaseException):
     pass
 class email_incorrecto (BaseException):
     pass
+#Con esto podemos terminar nuestra función que comprueba el email
 def check_email(cadena): #Definiremos una función que nos permita comprobar los requesitos 
     #Que la entrada es correcta
     if cadena is None or cadena == "": #Nuestra cadena no puede estar vacía
-        #acabar
+        raise entrada
+
     #Que el texto introducid tiene formato de correo electrónic
     text = cadena
     pattern = r"[a-z0-9-_]+[@]+[a-z]+[.]+[a-z]{2,}" 
     if (re.search(pattern, text)) is None: 
-        #acabar
+        raise formato_email
+        
     #Que el correo introducido es el correcto
     if cadena != "vincent@eni.es":
-        #Acabar
+        raise email_incorrecto
+
