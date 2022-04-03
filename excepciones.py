ecc391940b43bca33eb9d1ba4b0c25749283afb8
acasasaez <acasasae@myuax.com>
2022-04-03 12:09:55 +0200

    escribimos los requisitos de nuestro programa, lo que nos permitirá ir elaborándolo

---
+++ excepciones.py
@@ -0,0 +1,6 @@
+import re 
+#Tenemos que elaborar un programa que nos indique si la dirección de correo electrónico introducida es correcta
+#para esto debemos comprobar 
+#Que la entrada es correcta
+#Que el texto introducid tiene formato de correo electrónic 
+#Que el correo introducido es el correcto
