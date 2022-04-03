a70a5a6aa909f5e9741bd76d92186b50a9434685
acasasaez <acasasae@myuax.com>
2022-04-03 13:10:46 +0200

    Update excepciones.py

--- excepciones.py
+++ excepciones.py
@@ -5,13 +5,20 @@ import re
 #Que el texto introducid tiene formato de correo electrónic 
 #Que el correo introducido es el correcto
 
+#Creación de clases que sedrán las excepciones de nuestro programa
+class entrada (BaseException):
+    pass
+class formato_email (BaseException):
+    pass
+class email_incorrecto (BaseException):
+    pass
 def check_email(cadena): #Definiremos una función que nos permita comprobar los requesitos 
     #Que la entrada es correcta
     if cadena is None or cadena == "": #Nuestra cadena no puede estar vacía
         #acabar
     #Que el texto introducid tiene formato de correo electrónic
     text = cadena
-    pattern = 
+    pattern = r"[a-z0-9-_]+[@]+[a-z]+[.]+[a-z]{2,}" 
     if (re.search(pattern, text)) is None: 
         #acabar
     #Que el correo introducido es el correcto
